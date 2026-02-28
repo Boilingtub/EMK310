3c1f67b0c2995b0b8dcfd240ebc40f7ecb9468e6
Jan-Hendrik <jhbrink59@gmail.com>
2026-02-28 12:18:08 +0200

    PWM implementation of calibration

--- Practical_1.X/calibrate_test.s
+++ Practical_1.X/calibrate_test.s
@@ -4,6 +4,7 @@ PROCESSOR   18F45K22
 ;CONFIG1H
 CONFIG FOSC = INTIO67
 CONFIG WDTEN = OFF
+CONFIG CCP3MX = PORTE0
    
 #include    <xc.inc>
 #include    "pic18f45k22.inc"
@@ -13,6 +14,7 @@ CONFIG WDTEN = OFF
         
 PSECT code,abs	; Start Code section
 org	0h	; startup address = 0000h
+call setup
 goto main
 org 08h ;High priority Interrupt Vector
 goto ISRH
@@ -20,18 +22,20 @@ goto ISRH
 org 18h	;Low priority Interrupt Vector
 goto ISRL
  
-org 20h	;Start for code setup
- 
+org 20h	;Start for code setup 
 #include "setup.inc"
 #include "timer.inc"
 #include "interrupts.inc"
-#include "calibration.inc"
+#include "calibration_PWM_impl.inc"
 #include "line_location_interpreter.inc"
  
-main:   
-   call setup
-   call RGB_calibrate_test
+main: 
+   flash_Reg flash_counter, 3, DUMP_REG, 0b11111111
+   call pwm_setup
+   call calibrate
+   call calibrate_test_cont
    bra exit
    
 exit:
+    nop
     bra $-2
