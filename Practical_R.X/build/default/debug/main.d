fe697336cb731a8cf61b7a2d696aebf85504c8a6
Jan-Hendrik <jhbrink59@gmail.com>
2026-05-21 09:02:07 +0200

    Race day code

---
+++ Practical_R.X/build/default/debug/main.d
@@ -0,0 +1,4 @@
+build/default/debug/main.i: main.s constants.inc setup.inc pwm_setup.inc \
+  timer.inc Sensor.inc touch.inc color_detection.inc interrupts.inc \
+  calibration.inc line_location_interpreter.inc eeprom.inc serial.inc \
+  modes.inc simulate.inc
